e7b0699b1b38cd7157ff55743e04407e5710bcf1
KolosTK <94901694+KolosTK@users.noreply.github.com>
2023-11-02 18:50:41 +0200

    Add getters and setters to Item

--- src/main/java/org/example/Item.java
+++ src/main/java/org/example/Item.java
@@ -1,11 +1,36 @@
 package org.example;
 
 abstract class Item {
-    private String title;
-    private String uniqueID;
+    Item()
+    {
+        uniqueID++;
+    }
+    public String title;
+    private static int uniqueID = 0;
     private boolean isBorrowed = false;
 
     public abstract void borrowItem();
     public abstract void returnItem();
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getUniqueID() {
+        return uniqueID;
+    }
+
+
+
+    public boolean isBorrowed() {
+        return isBorrowed;
+    }
+
+    public void setBorrowed(boolean borrowed) {
+        isBorrowed = borrowed;
+    }
 }
