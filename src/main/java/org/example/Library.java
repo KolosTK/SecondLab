1fbbb49147e2e9f47ef46d4d4b779d68eac43424
KolosTK <94901694+KolosTK@users.noreply.github.com>
2023-11-05 13:17:45 +0200

    Fill add and remove methods in class Library

--- src/main/java/org/example/Library.java
+++ src/main/java/org/example/Library.java
@@ -1,9 +1,10 @@
 package org.example;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Library implements Manageable{
-    private List<Item> items;
-    private List <Customer> customers;
+    private List<Item> items = new ArrayList<Item>();
+    private List <Customer> customers=new ArrayList<Customer>();
 
     public void RegisterCustomer(Customer customer)
     {
@@ -19,13 +20,17 @@ public class Library implements Manageable{
     }
     public void add(Item item)
     {
-
+        items.add(item);
     }
     public void remove(Item item)
     {
-
+        items.remove(item);
     }
     public void ListBorrowed()
+    {
+
+    }
+    public void ListAvailable()
     {
 
     }
